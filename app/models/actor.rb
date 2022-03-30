8666e128c23e3bebce11aeff38a1ce2d90f6e6d6
Adam <adamlenter1@gmail.com>
2022-03-30 12:37:45 -0400

    commit tree a7ff727e37ed33f80f4a851517060a02def781e0

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,19 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    roles = self.characters
+    
+    list_of_roles = []
+
+    roles.each do |role|
+        list_of_roles << "#{role.name} - #{role.show.name}"
+    end
+    list_of_roles
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,16 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    actors = self.actors
+    list_of_actors = []
+
+    actors.each do |actor|
+        list_of_actors << "#{actor.first_name} #{actor.last_name}"
+    end
+
+    list_of_actors
+  end
 end
